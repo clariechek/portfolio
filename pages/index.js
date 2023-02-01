c5332c1f8d4c29ee2201215d0a578892e53fdfb5
ClarieChek <chek.clarie@gmail.com>
2023-02-01 12:04:17 +1030

    edited avatar

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,8 @@ export default function Home() {
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Hi, I'm Clarie!</h2>
             <h3 className='text-2xl py-2 md:text-3xl dark:text-gray-50'>Developer | Student</h3>
-            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
-              <Image src={avatar} layout='fill' objectFit='cover' alt="avatar image" />
+            <div className='relative mx-auto border-teal-500 border rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+              <Image src={avatar} className='object-fill' alt="avatar image" />
             </div>
           </div>
           <div className='text-center p-10'>
